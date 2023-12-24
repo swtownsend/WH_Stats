8b7b5c7144e5a144a2079e1c7db175c275fbdb36
scott <swtownsend@protonmail.com>
2023-12-24 12:41:34 -0500

    Lethal Hit

--- RsltAnalysis.py
+++ RsltAnalysis.py
@@ -19,7 +19,7 @@ def create_bargraph(weapon_df, hit_results_df, wounds_results_df, save_results_d
     avg_save_results_df.plot(x="Name",rot=0, title="Avg Amt of damage by Weapon",kind='bar')
 
     dmg_2v_df = avg_save_results_df[['Name',"Save 2+ vs Wound 2+","Dmg 2v2","Save 3+ vs Wound 2+","Dmg 3v2",
-                                 "Save 4+ vs Wound 2+","Dmg 4v2","Save 4+ vs Wound 2+","Dmg 5v2","Save 6+ vs Wound 2+",
+                                 "Save 4+ vs Wound 2+","Dmg 4v2","Save 5+ vs Wound 2+","Dmg 5v2","Save 6+ vs Wound 2+",
                                  "Dmg 6v2","Dev Wounds","Dev Wounds Dmg"]].copy()
 
     dmg_2v_df.plot(x="Name", rot=0, title="Avg Amt of damage by Weapon(Invuln saves wounding on 2+)", kind='bar')
